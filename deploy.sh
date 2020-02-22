b10012293a927ba26cc2d554053b15f47c223df3
java5575 <newman0934@gmail.com>
2020-02-22 23:37:11 +0800

    edit deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:newman0934/wapd-frontend.git master:gh-pages
+git push -f git@github.com:newman0934/wapd.git master:gh-pages
 
 cd -
